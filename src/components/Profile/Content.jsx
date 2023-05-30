bef09c5ad91784fbc8ddaf000216cb969c0b93cf
biri <annayewm1756@gmail.com>
2023-05-30 15:58:40 +0500

    добавил main content image

--- src/components/Profile/Content.jsx
+++ src/components/Profile/Content.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import s from './Content.module.css'
+import image from './../../img/main.png'
 
 
 const Content  = () => {
     return (
         <div className={s.content}>
+            <img className={s.main_img} src={image} alt="" />
+            
 
             
 
